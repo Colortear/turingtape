891ae1bc30eb86cad892c8e3ff232b7f275ec104
colortear <wdebs91@gmail.com>
2018-04-08 16:12:13 -0700

    refactoring machine module for clarity

--- src/machine.ml
+++ src/machine.ml
@@ -17,31 +17,55 @@ module type MACHINE =
     val trans : s
     val validate_json : unit -> int
   end
+
+let member_to_string json name =
+  match json |> member name |> to_string with
+  | Type_error -> ""
+  | x -> x
+
+let member_to_list json name =
+  json |> member name |> to_list |> filter_string
  
 module MachineMake : MACHINE = functor () ->
   struct
     type dir = Left | Right
     type s =
       | Nil
-      | Trans of string * ((string * string * string * dir) list ) list
+      | Trans of
+          (string * ((string * string * string * dir) list) Hashtble.t
+     (* | Trans of ( string * ((string * string * string * dir) list )) list*)
     let json_dump = Yojson.Basic.from_file Sys.argv.(1)
     let name =
       match json_dump |> member "name" |> to_string with
       | Type_error -> ""
+      | x -> x
     let alphabet = json_dump |> member "alphabet" |>
                    to_list |> filter_string
     let blank =
       match json_dump |> member "blank" |> to_string with
       | Type_error -> ""
+      | x -> x
     let states = json_dump |> member "states" |>
                  to_list |> filter_string
     let initial =
       match json_dump |> member "initial" |> to_string with
       | Type_error -> ""
+      | x -> x
     let finals = json_dump |> member "finals" |>
                  to_list |> filter_string
     let trans =
-      let rec aux =
+      let tbl = Hashtbl.create 1720 in
+      let transitions = json |> member "transitions" |> to_list in
+      let rec aux table =
+        match transitions with
+        | [] -> table
+        | hd::tl ->
+          let hd = hd |> member 
+          Hashtbl.add (aux table
+      in
+      let ret = aux tbl in
+      if ret = Null then Nil
+      else Trans(ret)
     let validate_json () =
       if json_dump = null || (String.length name = 0) ||
          alphabet = [] || (String.length blank != 1)
@@ -49,3 +73,5 @@ module MachineMake : MACHINE = functor () ->
          finals = [] || trans = Nil then -1
       else 1
   end
+
+module Machine : MACHINE = MachineMake()
